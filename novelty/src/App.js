40f632bb1aca86c78f714fd8cd680789a106fa6e
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-17 15:09:54 +0300

    book context

---
+++ novelty/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookContextProvider from "./contexts/BookContext";
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider />
+    </div>
+  );
+}
+
+export default App;
