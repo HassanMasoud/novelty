cafaf8e7c2ab88d4e2d5d80384c0d2f57570d13d
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-17 15:20:25 +0300

    navbar component and styling

--- novelty/src/App.js
+++ novelty/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import BookContextProvider from "./contexts/BookContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <BookContextProvider />
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
     </div>
   );
 }
